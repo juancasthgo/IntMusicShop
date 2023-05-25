4802c2b4040f7a992700a170e38fc7fb8b9e6c89
Juan Pablo Castillo Hidalgo <ju.castillo@duocuc.cl>
2023-05-24 20:26:33 -0400

    update

--- JKSA/settings.py
+++ JKSA/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/home'
 LOGOUT_REDIRECT_URL = '/'
 
 
@@ -150,7 +150,7 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- productos/views.py
+++ productos/views.py
@@ -16,24 +16,20 @@ from carritos.models import Carrito
 from .models import Categoria, Producto
 
 def inicio(request):
-    productos13 = Producto.objects.all()[:3]
-    productos410 = Producto.objects.all()[3:10]
+    productos13 = Producto.objects.all()
     categorias = Categoria.objects.all()
     context = {
         'primeros_productos': productos13,
-        'segundos_productos': productos410,
         'categorias': categorias,
     }
     return render(request, "inicio.html")
 
 
 def home(request):
-    productos13 = Producto.objects.all()[:3]
-    productos410 = Producto.objects.all()[3:10]
+    productos13 = Producto.objects.all()
     categorias = Categoria.objects.all()
     context = {
         'primeros_productos': productos13,
-        'segundos_productos': productos410,
         'categorias': categorias,
     }
     return render(request, "home.html", context)
